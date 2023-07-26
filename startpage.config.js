cdd4c5014d7f36e2c9b318acd818109e4cc4f1a2
er2de2 <62346235+er2de2@users.noreply.github.com>
2023-07-26 19:39:45 +0200

    Create startpage.config.js

---
+++ startpage.config.js
@@ -0,0 +1,266 @@
+{
+	"username": "roman",
+	"theme": {
+		"backgroundColor": "#1e1e2e",
+		"windowColor": "#1e1e2e",
+		"glowColor": "#f5c2e7",
+		"white": "#cdd6f4",
+		"gray": "#6c7086",
+		"black": "#313244",
+		"red": "#f38ba8",
+		"green": "#a6e3a1",
+		"blue": "#89b4fa",
+		"yellow": "#f9e2af",
+		"cyan": "#94e2d5",
+		"magenta": "#74c7ec",
+		"violet": "#cba6f7",
+		"orange": "#fab387"
+	},
+	"wallpaper": {
+		"url": "https://raw.githubusercontent.com/er2de2/test/main/waves.png",
+		"easing": "ease-in-out",
+		"fadeIn": true,
+		"blur": false
+	},
+	"terminal": {
+		"fixedHeight": true,
+		"windowGlowBrightness": "0.20",
+		"windowGlow": true,
+		"textGlow": false
+	},
+	"prompt": {
+		"ctrlC": true,
+		"placeholder": "wpisz...",
+		"placeholderColor": "white",
+		"userColor": "yellow",
+		"atColor": "blue",
+		"hostColor": "green",
+		"promptColor": "white",
+		"promptSymbol": "=>",
+		"caretColor": "green",
+		"selectionBg": "yellow",
+		"selectionFg": "black"
+	},
+	"fetch": {
+		"timeFormat": "HH:mm",
+		"dateFormat": "DD/MM/YYYY",
+		"titleColor": "yellow",
+		"image": "https://github.com/er2de2/test/raw/main/1544x1544_circle.png",
+		"data": [
+			"Godzina: {time}",
+			"Data: {date}",
+			"{seperator}",
+			"OS: {osName} {osVersion}",
+			"Przegladarka: {browser} {browserVersion}",
+			"{seperator}",
+			"{colors}"
+		]
+	},
+	"urlLaunch": {
+		"target": "_blank",
+		"defaultColor": "white",
+		"hoverColor": "violet"
+	},
+	"search": {
+		"default": "https://google.com/search?q=",
+		"target": "_blank",
+		"shortcutRegex": "([A-Za-z0-9]+) (.*)",
+		"shortcuts": [
+			{
+				"alias": "g",
+				"name": "Google Search",
+				"url": "https://google.com/search?q={}"
+			},
+			{
+				"alias": "d",
+				"name": "DuckDuckGo Search",
+				"url": "https://duckduckgo.com/?q={}"
+			}
+		]
+	},
+	"sections": {
+		"list": [
+			{
+				"title": "Linux",
+				"target": "_blank",
+				"color": "green",
+				"links": [
+					{
+						"name": "Mega",
+						"target": "_blank",
+						"url": "https://mega.nz/login",
+						"icon": "cib:mega"
+					},
+					{
+						"name": "Arch",
+						"target": "_blank",
+						"url": "https://archlinux.org/",
+						"icon": "simple-icons:archlinux"
+					},
+					{
+						"name": "Tecmint",
+						"target": "_blank",
+						"url": "https://www.tecmint.com/",
+						"icon": "raphael:linux"
+					},
+					{
+						"name": "UbuntuPIT",
+						"target": "_blank",
+						"url": "https://www.ubuntupit.com/",
+						"icon": "bi:ubuntu"
+					}
+				]
+			},
+			{
+				"title": "Filmy",
+				"color": "magenta",
+				"links": [
+					{
+						"name": "Serialowa",
+						"target": "_blank",
+						"url": "https://www.serialowa.pl/",
+						"icon": "material-symbols:live-tv-outline"
+					},
+					{
+						"name": "Darkbox",
+						"target": "_blank",
+						"url": "https://darkbox.vip/",
+						"icon": "game-icons:pirate-skull"
+					},
+					{
+						"name": "Files4You",
+						"target": "_blank",
+						"url": "https://www.files4you.org/",
+						"icon": "mdi:pirate"
+					},
+					{
+						"name": "tb7",
+						"target": "_blank",
+						"url": "https://tb7.pl/mojekonto",
+						"icon": "carbon:ibm-cloud-dedicated-host"
+					}
+				]
+			},
+			{
+				"title": "Mix",
+				"color": "orange",
+				"links": [
+					{
+						"name": "Darkmachine",
+						"target": "_blank",
+						"url": "https://darkmachine.pl",
+						"icon": "game-icons:pirate-skull"
+					},
+					{
+						"name": "Exsite24",
+						"target": "_blank",
+						"url": "https://exsite24.org/",
+						"icon": "game-icons:pirate-skull"
+					},
+					{
+						"name": "Github",
+						"target": "_blank",
+						"url": "https://github.com/login",
+						"icon": "tabler:brand-github-filled"
+					},
+					{
+						"name": "Vercel",
+						"target": "_blank",
+						"url": "https://vercel.com/login",
+						"icon": "tabler:brand-vercel"
+					}
+				]
+			},
+			{
+				"title": "Google",
+				"color": "blue",
+				"links": [
+					{
+						"name": "Youtube",
+						"target": "_blank",
+						"url": "https://www.youtube.com/feed/subscriptions",
+						"icon": "carbon:logo-youtube"
+					},
+					{
+						"name": "Gmail",
+						"target": "_blank",
+						"url": "https://mail.google.com/mail/u/0/?hl=pl&pli=1#inbox",
+						"icon": "mdi:gmail"
+					},
+					{
+						"name": "ProtonMail",
+						"target": "_blank",
+						"url": "https://account.proton.me/login",
+						"icon": "simple-icons:protonmail"
+					},
+					{
+						"name": "BSG",
+						"target": "_blank",
+						"url": "https://online.bsgoleniow.pl/",
+						"icon": "mdi:bank"
+					}
+				]
+			},
+			{
+				"title": "Reszta",
+				"color": "yellow",
+				"links": [
+					{
+						"name": "Reddit",
+						"target": "_blank",
+						"url": "https://www.reddit.com/new/",
+						"icon": "ic:baseline-reddit"
+					},
+					{
+						"name": "mBank",
+						"target": "_blank",
+						"url": "https://online.mbank.pl/pl/Login",
+						"icon": "ph:bank-fill"
+					},
+					{
+						"name": "eko.one",
+						"target": "_blank",
+						"url": "https://eko.one.pl/",
+						"icon": "simple-icons:openwrt"
+					},
+					{
+						"name": "Cloudflare",
+						"target": "_blank",
+						"url": "https://dash.cloudflare.com/login",
+						"icon": "simple-icons:cloudflare"
+					}
+				]
+			},
+			{
+				"title": "Media",
+				"color": "red",
+				"links": [
+					{
+						"name": "Viaplay",
+						"target": "_blank",
+						"url": "https://viaplay.pl/pl-pl/",
+						"icon": "solar:play-circle-outline"
+					},
+					{
+						"name": "Canal+",
+						"target": "_blank",
+						"url": "https://www.canalplus.com/pl/",
+						"icon": "mdi:plus-circle-outline"
+					},
+					{
+						"name": "Release24",
+						"target": "_blank",
+						"url": "http://release24.pl/",
+						"icon": "mdi:hours-24"
+					},
+					{
+						"name": "Jellyfin",
+						"target": "_blank",
+						"url": "http://fujitsu.dom.lilobanasik.ovh:8096",
+						"icon": "simple-icons:jellyfin"
+					}
+				]
+			}
+		]
+	}
+}
